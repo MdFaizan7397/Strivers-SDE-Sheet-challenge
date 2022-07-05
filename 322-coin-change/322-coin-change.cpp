479a5a52705264684b3b6c2d5d2e63e71422e393
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-07-05 18:38:58 +0530

    Time: 269 ms (10.76%), Space: 14.5 MB (57.98%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -26,7 +26,7 @@ public:
 };*/
 
 
-class Solution {  //MEMOIZATION
+/*class Solution {  //MEMOIZATION
 private:
     int solve(vector<int>&coins,int amount, vector<int>&dp) {
         //BASE CASE
@@ -55,5 +55,33 @@ public:
             return -1;
         
         return ans;
+    }  
+};*/
+
+
+class Solution {  //TABULATION
+private:
+    int solve(vector<int>&coins,int amount) {
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX){
+                    
+                
+                dp[i]=min(dp[i],1+dp[i-coins[j]]);
+                }
+            }
+        }
+        if(dp[amount]==INT_MAX){
+            return -1;
+        }
+        return dp[amount];
+        
+        
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {        
+        return solve(coins,amount);
     }  
 };
