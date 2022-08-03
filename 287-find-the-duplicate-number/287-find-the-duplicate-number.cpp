8332033cf94b63452d1765586e533714e24f8f0c
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-03 18:03:18 +0530

    Time: 214 ms (39.44%), Space: 61.2 MB (61.46%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-       sort(nums.begin(),nums.end());
-        int i;
-        for(i=0;i<n;i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
-            }
-        }
-        return-1;
+        //TORTOISE METHOD  //Optimized Approach
+        //using slow and fast pointer
+        //slow moves by 1 step
+        //fast moves by 2 steps
         
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            
+        }while(slow!=fast);
+        
+        //Now here slow and fast both moves by 1 step
+        //intialise fast = nums[0];
+          fast=nums[0];
+          while(slow!=fast){
+              slow=nums[slow];
+              fast=nums[fast];
+          }
+        return slow;
     }
 };
