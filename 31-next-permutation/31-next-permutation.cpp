e19350e69b5ff224dc9450309af86737c05ea545
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-05 07:25:34 +0530

    Time: 7 ms (58.73%), Space: 12.1 MB (36.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,72 +1,25 @@
-//https://www.youtube.com/watch?v=6qXO72FkqwM
-/*class Solution {
-public:
-    void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return;
-        
-        int i=1;
-        int lastInc=-1; //Right most is nothing but the lastIncuded.
-        while(i<n){  //Find the peak of last ascending order element.
-            if(nums[i]>nums[i-1]){
-                lastInc=i;
-            }
-            i+=1;
-            
-        }
-        if(lastInc==-1){ //check if array is in decending order,than there will be no peaks.
-                         //then simply sort the array and return.
-            for(i=0;i<n/2;i++){
-                swap(nums[i],nums[n-i+1]); //swapping the array if no peak is found.
-            }
-            return;
-            
-        }
-        
-        // SPECIAL CASE 
-        //Find the element in the range (nums[lastInc-1] to nums[lastInc]) to the right.
-        int mn=nums[lastInc];
-        int index= lastInc;
-        for(i=lastInc;i<n;i++){
-            if(nums[i]>nums[lastInc-1] and nums[i]<nums[index]){ //move to the descending curve at right and see if any element is smaller than  the given peak than that peak will be replaced by that element.
-                index=i;
-            }
-        }
-        swap(nums[lastInc-1],nums[index]);
-        sort(nums.begin()+lastInc,nums.end());
-        
-    }
-};*/
-
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1)
-            return;
-        
-        int i=1;
-        int lastInc = -1;
-        while(i<n) {    //Find the peak of last ASC order
-            if(nums[i]>nums[i-1])
-                lastInc = i;
-            i+=1;
-        }
-        
-        if(lastInc==-1) {   //Check if array is DSC
-            for(i=0;i<n/2;i++)
-                swap(nums[i],nums[n-i-1]);
-            return;
-        }
-        //Find element in the range (nums[lastInc-1] to nums[lastInc]) to the right
-        int mn = nums[lastInc];
-        int index = lastInc;
-        for(i=lastInc;i<n;i++) {
-            if(nums[i]>nums[lastInc-1] and nums[i]<nums[index]) {
-                index = i;
+        int n=nums.size();
+        int k,l;
+        for(k=n-2;k>=0;k--){
+            if(nums[k]<nums[k+1]){
+                break;
             }
         }
-        swap(nums[lastInc-1],nums[index]);
-        sort(nums.begin()+lastInc,nums.end());
+        if(k<0){
+            reverse(nums.begin(),nums.end());
+            
+        }
+        else {
+            for(l=n-1;l>k;l--){
+                if(nums[l]>nums[k]){
+                    break;
+                }
+            }
+            swap(nums[l],nums[k]);
+            reverse(nums.begin()+k+1,nums.end());
+        }
     }
 };
