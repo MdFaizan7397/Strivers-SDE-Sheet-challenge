d7a1cdb01780f866d924a535b394389ee8b41a75
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-14 11:36:10 +0530

    Time: 2 ms (66.41%), Space: 8.2 MB (90.52%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//OPTIMIZED APPROACH
+/*class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int zero=0,one=0,two=0;
@@ -20,6 +21,37 @@ public:
             else{
                 nums[i] = 2;
                 two--;
+            }
+        }
+    }
+};*/
+//BEST APPROACH : DUTCH NATIONAL FLAG ALGORITHM
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        //we need 3 pointers
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        
+        while(mid<=high){
+            switch(nums[mid]){
+                
+                //when mid pointer is pointing to 0
+                case 0: swap(nums[low],nums[mid]);
+                mid++;
+                low++;
+                break;
+                
+                //when mid pointer is pointing to 1
+                case 1: mid++;
+                break;
+                
+                //when mid pointer is pointing to 2
+                case 2 : swap(nums[high],nums[mid]);
+                high--;
+                break;
             }
         }
     }
