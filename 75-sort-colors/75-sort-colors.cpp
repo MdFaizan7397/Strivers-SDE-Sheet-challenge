d2731f753aab715a5e91734e2264e2ea583f4058
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-14 11:34:13 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (70.34%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
-        //we need 3 pointers
-        int low=0;
-        int mid=0;
-        int high=n-1;
+        int zero=0,one=0,two=0;
+        for(int i : nums){
+            if( i == 0) zero++;
+            if( i == 1) one++;
+            if( i == 2) two++;
+        }
         
-        while(mid<=high){
-            switch(nums[mid]){
-                
-                //when mid pointer is pointing to 0
-                case 0: swap(nums[low],nums[mid]);
-                mid++;
-                low++;
-                break;
-                
-                //when mid pointer is pointing to 1
-                case 1: mid++;
-                break;
-                
-                //when mid pointer is pointing to 2
-                case 2 : swap(nums[high],nums[mid]);
-                high--;
-                break;
+        for(int i = 0; i<nums.size(); i++){
+            if( zero != 0){
+                nums[i] = 0;
+                zero--;
+            }
+            else if( one != 0){
+                nums[i] = 1;
+                one--;
+            }
+            else{
+                nums[i] = 2;
+                two--;
             }
         }
     }
