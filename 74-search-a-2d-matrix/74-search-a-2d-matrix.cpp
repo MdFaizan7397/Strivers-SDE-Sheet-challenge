f89bc4ee3df07352c2badaea44854462714a2456
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-16 22:18:10 +0530

    Time: 9 ms (33.84%), Space: 9.6 MB (51.70%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//BRUTE FORCE
+/*class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n=matrix.size();
@@ -10,6 +11,31 @@ public:
                     return true;
                 }
             }
+        }
+        return false;
+        
+    }
+};*/
+//OPTIMIZED APPROACH : BINARY SEARCH 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int low=0;
+        int high=(m*n)-1;
+        while(low<=high){
+            int mid=(low+(high-low)/2);
+            if(matrix[mid/m][mid%m]==target){
+                return true;
+            }
+            else if(matrix[mid/m][mid%m]<target){
+                 low=mid+1;
+            }
+            else {
+               high=mid-1;
+            }
+            
         }
         return false;
         
