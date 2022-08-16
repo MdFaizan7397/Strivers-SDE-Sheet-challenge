5a05c06435ba714de3d66c567249dc2f600cd6b9
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-16 18:52:22 +0530

    Time: 78 ms (30.71%), Space: 10.7 MB (44.39%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,5 +1,5 @@
 //BRUTE FORCE
-class Solution {
+/*Class Solution {
 public:
     int firstUniqChar(string s) {
         unordered_map<char, int> m;
@@ -10,5 +10,23 @@ public:
             if (m[s[i]] == 1) return i;
         }
         return -1;
+    }
+};*/
+//OPTIMAL
+class Solution {
+public:
+	int firstUniqChar(string s) {
+        unordered_map<char, pair<int, int>> m;
+        int idx = s.size();
+        for (int i = 0; i < s.size(); i++) {
+            m[s[i]].first++;
+            m[s[i]].second = i;
+        }
+        for (const auto& [c, p] : m) {
+            if (p.first == 1) {
+                idx = min(idx, p.second);
+            }
+        }
+        return idx == s.size() ? -1 : idx;
     }
 };
