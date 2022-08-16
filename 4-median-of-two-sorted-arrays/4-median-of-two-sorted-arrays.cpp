74cc417d7d64d811b00fca8cdb5bbcd0c0886079
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-16 07:02:08 +0530

    Time: 83 ms (19.86%), Space: 89.2 MB (86.59%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        if(n2<n1) 
+            return findMedianSortedArrays(nums2, nums1);
+         
         
-        int n=nums1.size();
-        int m=nums2.size();
-        vector<int>ans(n+m);
-        //int k=ans.size();
-    int a = 0;
-    for(int i = 0 ;i<n;i++)
-    {
-        ans[i] = nums1[i];
+        int low=0,high=n1;
+        while(low<=high){
+            int cut1=(low+high)>>1;
+            int cut2=(n1+n2+1)/2 -cut1;
+            
+            int left1=cut1== 0 ? INT_MIN : nums1[cut1-1];
+            int left2=cut2== 0 ? INT_MIN : nums2[cut2-1];
+            
+            int right1=cut1== n1 ? INT_MAX : nums1[cut1];
+            int right2=cut2== n2 ? INT_MAX : nums2[cut2];
+            
+            if(left1<=right2 && left2<=right1){
+                if((n1+n2)%2==0)
+                    return (max(left1,left2) + min(right1,right2))/2.0;
+                else 
+                    return max(left1,left2);//if length is odd
+            }
+            else if(left1>right2){
+                high=cut1-1;
+            }
+            else {
+                low=cut1+1;
+            }
+             
+        }
+        return 0.0;
     }
-    
-    for(int i =n ;i<ans.size();i++)
-    {
-        ans[i] = nums2[a];
-        a++;
-    }
-    sort(ans.begin(),ans.end());
-    int mid = ans.size()/2;
-    if(ans.size()%2==1) return ans[mid];
-    else return (ans[mid] + ans[mid-1])/2.00000;
-}
-    
 };
