2c5dba654c37e0ccc072fe0b20b03136126a2335
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-21 10:03:57 +0530

    Time: 21 ms (99.57%), Space: 13.2 MB (60.49%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -16,22 +16,44 @@ public:
     }
 };*/
 
-//OPTIMAL APPROACH
+//OPTIMAL APPROACH :  HEAP: priority queue
 
-class Solution {
+/*class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-         priority_queue<int>p;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                 p.push(matrix[i][j]);  
-                if(p.size()>k){
-                    p.pop();
-                }
-            } 
-        }
-        return p.top();
+        priority_queue<int>pq; int n = matrix.size();
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++){
+                pq.push(matrix[i][j]);
+                if(pq.size() > k) pq.pop();
+            }
+        return pq.top();   
     }
+};*/
+
+//APPROACH 3 : BINARY SEARCH
+class Solution {
+public:
+    //from left-bottom or right-top, count how many numbers are less equal than mid
+    int solve(vector<vector<int>>& matrix, int mid){
+        int count = 0, n = matrix.size(), i = n-1, j = 0;
+        while(i >= 0 && j < n){
+            if(matrix[i][j] > mid) i--;
+            else{
+                count += (i+1);
+                j++;
+            }
+        }
+        return count;
+    }
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size(), i = matrix[0][0], j = matrix[n-1][n-1];
+        while(i < j){
+            int mid = i + (j-i)/2;
+            int posi = solve(matrix, mid);
+            if(posi < k) i = mid+1;
+            else j = mid;
+        }
+        return i;
+    }  
 };
