d404531cb9758b522c898717e53ef95d824bf741
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-21 08:59:33 +0530

    Time: 35 ms (87.33%), Space: 14.6 MB (25.72%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
-struct compare
-{
-    bool operator()(const pair<int,pair<int, int> >& a, const pair<int,pair<int, int> >& b)
-    {
-        return a.first>b.first;
-    }
-};
-    int kthSmallest(vector<vector<int>>& arr, int k) {
-        
-        int n=arr.size(),m=arr[0].size();
-        
-        priority_queue< pair<int,pair<int, int> >, vector<pair<int, pair<int, int> > >, compare > p;
-        
-        for(int i=0;i<n;i++)
-        p.push(make_pair(arr[i][0],make_pair(i,0)));
-        
-        int x=k,ans;
-        while(x--)
-        {
-            int e=p.top().first;
-            int i=p.top().second.first;
-            int j=p.top().second.second;
-            ans=e;
-            p.pop();
-            if(j!=m-1)
-            p.push(make_pair(arr[i][j+1],make_pair(i,j+1)));
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        long long int n = matrix.size();
+        long long int i,j;
+        vector<int>s;
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                long long int a = matrix[i][j];
+                s.push_back(a);  //Pushing the elements one by one in the vector s
+            }
         }
-        return ans;
-        
+        sort(s.begin(), s.end());
+        return s[k-1];   //k-1 is the index of the kth smallest number
     }
 };
