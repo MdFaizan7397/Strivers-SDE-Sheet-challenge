469f75f8d3089dd9a379c896865e5611a174c5fb
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-08-21 07:12:07 +0530

    Time: 4 ms (42.75%), Space: 7.1 MB (73.77%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,5 +1,5 @@
 //BRUTE FORCE
-class Solution {
+/*class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
            int n=matrix.size();
@@ -19,5 +19,23 @@ for(int i=0;i<n;i++)
             }
         }
 
+    }
+};*/
+
+//OPTIMIZED APPROACH
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+        
     }
 };
