3393f85bd67290a06acd2feb0a493911d9926f4b
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-10-08 07:57:36 +0530

    Time: 6 ms (17.06%), Space: 6.3 MB (6.84%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -0,0 +1,18 @@
+/*class Solution {
+public:
+    string replaceDigits(string s) {
+        for(int i=0;i<s.size();i=i+2){
+            s[i] = s[i] + (s[i-1] - '0');
+        }
+        return s;
+        
+    }
+};*/
+class Solution {
+public:
+    string replaceDigits(string s) {
+        for(int i=1;i<s.size();i+=2)
+               s[i]+=(s[i-1]-'0');
+        return s;
+    }
+};
