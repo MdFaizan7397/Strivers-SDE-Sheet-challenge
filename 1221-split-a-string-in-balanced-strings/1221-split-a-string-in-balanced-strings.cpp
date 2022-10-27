f4fe2738b0d50c3f1e30b333346b377fce539c66
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-10-27 18:15:51 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (38.36%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        int lcount=0;
-        int rcount=0;
-        int count=0;
         int n=s.length();
+        int l=0;
+        int r=0;
+        int count=0;
         for(int i=0;i<n;i++){
-            if(s[i]=='R'){
-                rcount++;
-            }
-            else {
-                lcount++;
-            }
-            if(lcount==rcount){
+                if(s[i]=='R'){
+                    r++;
+                }
+                else if(s[i]=='L'){
+                    l++;
+                }
+                if(l==r){
                 count++;
             }
             
+            
         }
         return count;
-        
-        
     }
 };
