1772d0dbdb5d09e5edf291a90f89e294e796f5e3
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-11-02 23:02:24 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (58.96%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
+        int n=s.length();
         int count=0;
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<n;i++){
             if(s[i]==' '){
                 count++;
             }
             if(count==k){
-                s.erase(s.begin()+i , s.end());
-            return s;
-            }
-            
-            
+            s.erase(s.begin()+i,s.end());
+                return s;
+         }
         }
-        return s ;
+        return s;
+        
     }
 };
+
