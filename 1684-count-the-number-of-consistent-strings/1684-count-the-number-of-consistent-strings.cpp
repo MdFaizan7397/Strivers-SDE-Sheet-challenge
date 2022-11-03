c0df07609bee9aeb896d79e6103a070d1dbe2e27
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2022-11-03 22:26:51 +0530

    Time: 120 ms (62.30%), Space: 30.3 MB (26.87%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        unordered_map<char,int> mp;
-    int ans = 0;
-    
-    for(int i=0;i<allowed.size();i++)
-    {
-        mp[allowed[i]]++;
-    }
-    int flag = true;
-    for(int i=0;i<words.size();i++)
-    {
-        for(int j=0;j<words[i].size();j++)
-        {   
-        if(mp.find(words[i][j])==mp.end()){
-            flag = false;
-            break;
-        }
-            
-                
-                
-        }
-        if(flag==true)
-            ans++;
-        flag=true;
         
-    }
-    return ans;
+        unordered_map<char,int>mp;
+        int count=0;
+        
+        for(int i=0;i<allowed.length();i++){
+            mp[allowed[i]]++;
+        }
+        
+        for(int i=0;i<words.size();i++){
+            
+            bool flag=true;
+            for(int j=0;j<words[i].size();j++){
+                if(mp.find(words[i][j])==mp.end()){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag==true){
+                count++;
+            }
+            
+        }
+        return count;
     }
 };
