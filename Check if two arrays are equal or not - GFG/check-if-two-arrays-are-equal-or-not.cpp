12ed67812eb874b2ea97df849e26724b641ef827
Md Faizan <89077589+MdFaizan7397@users.noreply.github.com>
2023-01-28 19:18:55 +0530

    Added solution - LeetHub

--- Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.cpp
+++ Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.cpp
@@ -12,19 +12,32 @@ class Solution{
     public:
 
     //Function to check if two arrays are equal or not.
+    
     bool check(vector<ll> A, vector<ll> B, int N) {
-        sort(A.begin(), A.end());
-        sort(B.begin(), B.end());
-        int count=0;
+        //all elements of array A is putten to Hashmap
+        unordered_map<int,int>mp;
         for(int i=0;i<N;i++){
-            if(A[i]==B[i]){
-                count++;
-                if(count==N){
-                    return 1;
+            mp[A[i]]++;
+        }
+        
+        //now here we will iterate the array B , 
+        //and find elements of B in the Hash map
+        for(int i=0;i<N;i++){
+            int key =B[i];
+            if(mp.find(key)!=mp.end()){ //finding element in the hashmap
+                auto itr =mp.find(key);
+                if(itr->second>0){
+                    itr->second--; //if element found decrement the count of key
+                }
+                else {
+                    return false;
                 }
             }
+            else {
+                return false;
+            }
         }
-        return 0;
+        return true;
     }
 };
 
